e1e39ddf45fbf1b2b74f776299ef7498085030df
prathnow <prathnow@gmail.com>
2023-02-05 12:50:16 +0100

    Add function to create username

--- script.js
+++ script.js
@@ -81,3 +81,9 @@ const displayMovements = function(movements) {
   })
 };
 
+const createUsernames = function(accs) {
+  accs.forEach(function(acc) {
+    acc.username = acc.owner.toLowerCase().split(' ').map(name  => name[0]).join('');
+  });
+};
+
