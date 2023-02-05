d469035f82abe261a4766c18ad0a8f364e63b07c
prathnow <prathnow@gmail.com>
2023-02-05 17:19:44 +0100

    Save code with Prettier

--- script.js
+++ script.js
@@ -64,26 +64,31 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //////////////////////////////////
 // Functions
 
-const displayMovements = function(movements) {
+const displayMovements = function (movements) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function(mov,i) {
+  movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
     <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
           <div class="movements__value">${mov}</div>
         </div>
     `;
 
-    containerMovements.insertAdjacentHTML('afterbegin', html)
-  })
-};
-
-const createUsernames = function(accs) {
-  accs.forEach(function(acc) {
-    acc.username = acc.owner.toLowerCase().split(' ').map(name  => name[0]).join('');
+    containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map((name) => name[0])
+      .join('');
+  });
+};
